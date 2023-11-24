40c9f969ac01df80786f22230eb02ad2b9baac41
Karel R. Varona <kwaref@gmail.com>
2023-11-24 11:48:29 +0000

    feat: 4.4 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,11 @@ const dummy = blogs => {
   return 1
 }
 
+const totalLikes = blogPosts => {
+  return blogPosts.reduce((sum, post) => sum + (post.likes || 0), 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
 }
