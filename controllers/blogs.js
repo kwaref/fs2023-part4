9f5b3eab6c95155d3db21c580526da82fa461060
Karel R. Varona <kwaref@gmail.com>
2023-11-27 23:54:35 +0000

    feat: 4.18 - 4.20 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/info', async (_, response) => {
   const blogs = await Blog.find({})
@@ -39,33 +40,36 @@ blogsRouter.put('/:id', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
   const { title, author, url, likes } = body
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+
   if (!title) {
     return response.status(400).json({
       error: 'title is missing',
     })
   }
-  // if (!author) {
-  //   return response.status(400).json({
-  //     error: 'author is missing',
-  //   })
-  // }
   if (!url) {
     return response.status(400).json({
       error: 'url is missing',
     })
   }
 
-  const users = await User.find({})
-
   const blog = new Blog({
     title,
     author,
     url,
     likes: likes ?? 0,
-    user: users[0].id
+    user: user._id
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
